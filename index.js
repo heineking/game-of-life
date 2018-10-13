6181ad009c7784d8a87c173d3711aeec05abe786
Emil Heineking <ebheineking@gmail.com>
2018-10-13 18:36:59 -0400

    refactored index to remove class

--- index.js
+++ index.js
@@ -2,63 +2,54 @@
 const repeat = (f, n) => Array(n).fill(0).map((_, i) => f(i));
 const cell = (x, y) => ({x, y});
 
-class GameOfLife {
-  static createGrid(w, h, state) {
-    return repeat(() => repeat(state, w), h);
+function createGrid(w, h, state) {
+  return repeat(() => repeat(state, w), h);
+}
+
+const forEachCell = (grid, f) => {
+  for(let x = 0; x < grid[0].length; ++x) {
+    for (let y = 0; y < grid.length; ++y) {
+      f(x, y, grid[x][y]);
+    }
   }
-  static neighbors({x, y}, {h, w}) {
-    const coordinates = [
-      ...repeat(i => cell(x-1+i,y-1), 3),
-      ...repeat(i => cell(x-1+i, y), 3),
-      ...repeat(i => cell(x-1+i, y+1), 3),
-    ];
-    return coordinates
-      .filter(xy => xy.x !== x || xy.y !== y)
-      .filter(xy => xy.x >= 0 && xy.y >= 0)
-      .filter(xy => xy.x < w && xy.y < h);
-  }
-  static nextCellState(current, neighbors) {
-    return current
-      ? neighbors === 2 || neighbors === 3
-      : neighbors === 3;
-  }
-  constructor(grid) {
-    // private vars
-    const $grid = JSON.parse(JSON.stringify(grid));
+};
 
-    const cells = (f) => {
-      for(let x = 0; x < $grid[0].length; ++x) {
-        for (let y = 0; y < $grid.length; ++y) {
-          f(x, y, $grid[x][y]);
-        }
-      }
-    };
+function neighbors({x, y}, {h, w}) {
+  const coordinates = [
+    ...repeat(i => cell(x-1+i,y-1), 3),
+    ...repeat(i => cell(x-1+i, y), 3),
+    ...repeat(i => cell(x-1+i, y+1), 3),
+  ];
+  return coordinates
+    .filter(xy => xy.x !== x || xy.y !== y)
+    .filter(xy => xy.x >= 0 && xy.y >= 0)
+    .filter(xy => xy.x < w && xy.y < h);
+}
 
-    // utilities
-    this.countOfLiveNeighbors = (cell) => {
-      return GameOfLife
-        .neighbors(cell, {h: $grid.length + 1, w: $grid[0].length + 1})
-        .reduce((count, { x, y }) => count += $grid[x][y], 0);
-    };
+function countOfLiveNeighbors(grid, cell) {
+  return neighbors(cell, { h: grid.length, w: grid[0].length })
+    .reduce((count, { x, y }) => count += grid[x][y], 0);
+}
 
-    const nextGeneration = () => {
-      const h = $grid.length;
-      const w = $grid[0].length; 
-      const nextGrid = GameOfLife.createGrid(w, h, () => false);
-      cells((x, y, current) => {
-        const neighbors = this.countOfLiveNeighbors({ x, y });
-        nextGrid[x][y] = GameOfLife.nextCellState(current, neighbors);
-      });
-      $grid = nextGrid;
-    };
+function nextCellState(current, neighbors) {
+  return current
+    ? neighbors === 2 || neighbors === 3
+    : neighbors === 3;
+}
 
-    this.tick = (next) => {
-      this.nextGeneration();
-      cells(next);
-    };
-  };
+function generateNextGrid(grid) {
+  const h = grid.length;
+  const w = grid[0].length;
+  const next = createGrid(grid[0].length, grid.length, () => false);
+  forEachCell(grid, (x, y, current) => {
+    next[x][y] = nextCellState(current, neighbors({ x, y }, { h, w }));
+  });
+  return next;
 }
 
 module.exports = {
-  GameOfLife
+  createGrid,
+  neighbors,
+  nextCellState,
+  countOfLiveNeighbors
 };
--- tests.js
+++ tests.js
@@ -1,39 +1,37 @@
 const { expect } = require('chai');
-const { GameOfLife } = require('./index');
+const {
+  createGrid,
+  neighbors,
+  nextCellState,
+  countOfLiveNeighbors,
+} = require('./index');
 
 // time: 16:50
 describe('GameOfLife', () => {
-  it('should have a working test environment', () => {
-    expect(true).to.equal(true);
-  });
-  it('should be a class', () => {
-    expect(GameOfLife.toString()).to.contain('class GameOfLife');
-  });
   describe('#countOfLiveNeighbors', () => {
     it('should return the count of live neighbors', () => {
-      const grid = GameOfLife.createGrid(10, 10, () => false);
+      const grid = createGrid(10, 10, () => false);
       grid[0][0] = true;
-      const game = new GameOfLife(grid);
-      const count = game.countOfLiveNeighbors({ x: 1, y: 1 });
+      const count = countOfLiveNeighbors(grid, { x: 1, y: 1 });
       expect(count).to.equal(1);
     });
   });
   describe('#nextCellState', () => {
     it('should return false if current state is true and neighbors is less than two', () => {
-      expect(GameOfLife.nextCellState(true, 1)).to.equal(false);
-      expect(GameOfLife.nextCellState(true, 0)).to.equal(false);
+      expect(nextCellState(true, 1)).to.equal(false);
+      expect(nextCellState(true, 0)).to.equal(false);
     });
     it('should return true if current state is true and neighbors is two or three', () => {
-      expect(GameOfLife.nextCellState(true, 2)).to.equal(true);
-      expect(GameOfLife.nextCellState(true, 2)).to.equal(true);
+      expect(nextCellState(true, 2)).to.equal(true);
+      expect(nextCellState(true, 2)).to.equal(true);
     });
     it('should return true if current state is false and neighbors is three', () => {
-      expect(GameOfLife.nextCellState(false, 3)).to.equal(true);
+      expect(nextCellState(false, 3)).to.equal(true);
     });
   });
   describe('#createGrid', () => {
     it('should return a 2-dimensional array', () => {
-      const grid = GameOfLife.createGrid(10, 10, () => false);
+      const grid = createGrid(10, 10, () => false);
       expect(grid.length).to.equal(10);
       grid.forEach(row => expect(row.length).to.equal(10));
     });
@@ -42,28 +40,28 @@ describe('GameOfLife', () => {
     const cell = (x, y) => ({x, y});
     const rect = (h, w) => ({h, w});
     it('should return eight neigbhor coordinates when cell is not on a boundary', () => {
-      const neighbors = GameOfLife.neighbors(cell(1, 1), rect(3, 3));
-      expect(neighbors.length).to.equal(8);
+      const n = neighbors(cell(1, 1), rect(3, 3));
+      expect(n.length).to.equal(8);
     });
     it('should return three neighbor coordinates when cell is top-left', () => {
-      const neighbors = GameOfLife.neighbors(cell(0, 0), rect(3, 3));
-      expect(neighbors.length).to.equal(3);
+      const n = neighbors(cell(0, 0), rect(3, 3));
+      expect(n.length).to.equal(3);
     });
     it('should return three neighbor coordinates when cell is bottom-right', () => {
-      const neighbors = GameOfLife.neighbors(cell(1, 1), rect(2, 2));
-      expect(neighbors.length).to.equal(3);
+      const n = neighbors(cell(1, 1), rect(2, 2));
+      expect(n.length).to.equal(3);
     });
     it('should return five neighbor coordinates when cell is on top row', () => {
-      const neighbors = GameOfLife.neighbors(cell(0, 1), rect(3, 3));
-      expect(neighbors.length).to.equal(5);
+      const n = neighbors(cell(0, 1), rect(3, 3));
+      expect(n.length).to.equal(5);
     });
     it('should return three neighbor coordinates when cell is top-right', () => {
-      const neighbors = GameOfLife.neighbors(cell(2, 2), rect(3, 3));
-      expect(neighbors.length).to.equal(3);
+      const n = neighbors(cell(2, 2), rect(3, 3));
+      expect(n.length).to.equal(3);
     });
     it('should return three neighbor coordinates when cell is bottom-left', () => {
-      const neigbhors = GameOfLife.neighbors(cell(0, 2), rect(3, 3));
-      expect(neigbhors.length).to.equal(3);
+      const n = neighbors(cell(0, 2), rect(3, 3));
+      expect(n.length).to.equal(3);
     });
   });
 });
