d1372db38056c515705a09525aadd7fe81aca415
Emil Heineking <ebheineking@gmail.com>
2018-10-13 15:39:29 -0400

    refactored to use a GameOfLife class

--- index.js
+++ index.js
@@ -1,20 +1,13 @@
 'use strict';
-// index.js
-class Cell {
-  constructor(x, y, grid){
-    this.x = x;
-    this.y = y;
-    this.grid = grid;
-  }
 
-  alive() {
-    return this.grid[this.x][this.y];
+class GameOfLife {
+  static createGrid() {
+    return [];
   }
-
-  neighbors(){
-    return []
+  constructor() {
   }
 }
+
 module.exports = {
-  Cell,
+  GameOfLife
 };
--- tests.js
+++ tests.js
@@ -1,54 +1,18 @@
 const { expect } = require('chai');
-const { Cell } = require('./index');
+const { GameOfLife } = require('./index');
 
 // time: 16:50
-describe('tests.js', () => {
-  it('should work', () => {
+describe('GameOfLife', () => {
+  it('should have a working test environment', () => {
     expect(true).to.equal(true);
   });
-});
-
-describe('Cell', () => {
-  it('should be defined', () => {
-    expect(Cell).to.not.equal(undefined);
+  it('should be a class', () => {
+    expect(GameOfLife.toString()).to.contain('class GameOfLife');
   });
-
-  it('should be a class.', () => {
-    expect(Cell.toString()).contains('class Cell');
-  });
-
-  it('should have a x coordinate', () => {
-    const cell = new Cell(1);
-    expect(cell.x).to.equal(1);
-  });
-
-  it('should have a y coordinate', () => {
-    const cell = new Cell(1,1);
-    expect(cell.y).to.equal(1);
-  });
-
-  it('should take in a grid', () =>{
-    const cell = new Cell(1, 1, [[]])
-    expect(Array.isArray(cell.grid)).to.equal(true);
-  });
-
-  it('should know if it is alive', () => {
-    // arrange
-    const grid = [
-      [true]
-    ];
-    const y = 0;
-    const x = 0;
-
-    // act
-    const cell = new Cell(x, y, grid);
-
-    // assert
-    expect(cell.alive()).to.equal(true);
-  });
-
-  it('should maintain a list of neigbors', () => {
-    const cell = new Cell(0,0, [[]]);
-    expect(cell.neighbors()).to.be.a('Array');
+  describe('#createGrid', () => {
+    it('should return an array', () => {
+      const grid = GameOfLife.createGrid();
+      expect(grid).to.be.a('Array');
+    });
   });
 });
