1d43b9f89f24c136c97f85d3ae222ef92fced434
Emil Heineking <ebheineking@gmail.com>
2018-10-13 17:13:17 -0400

    refactored neighbors

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 'use strict';
 const repeat = (f, n) => Array(n).fill(0).map((_, i) => f(i));
+const cell = (x, y) => ({x, y});
 
 class GameOfLife {
   static createGrid(x, y, state) {
     return repeat(() => repeat(state, x), y);
   }
   static neighbors({x, y}, {h, w}) {
-    const cell = (x, y) => ({x, y});
     const coordinates = [
       ...repeat(i => cell(x-1+i,y-1), 3),
       ...repeat(i => cell(x-1+i, y), 3),
